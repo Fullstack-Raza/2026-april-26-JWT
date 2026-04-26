2228c4bf57cf5ef0bc985249e1877c56d242234c
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-04-26 15:18:01 +0500

    save token in cookie

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,15 @@
 const express = require("express");
-
 const usermodel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
-
 
 authRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const isuserexsist = await usermodel.findOne({email})
-  if(isuserexsist){
-return res.status(400).json({
-    message:"alredy exsists"
-})
+  const isuserexsist = await usermodel.findOne({ email });
+  if (isuserexsist) {
+    return res.status(400).json({
+      message: "alredy exsists",
+    });
   }
 
   const user = await usermodel.create({
@@ -18,9 +17,19 @@ return res.status(400).json({
     email,
     password,
   });
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email:user.email
+    },
+    process.env.JWT_secrets,
+  );
+
+  res.cookie("JWt_token",token)
   res.status(201).json({
     massage: "user registered",
     user,
+    token,
   });
 });
 
